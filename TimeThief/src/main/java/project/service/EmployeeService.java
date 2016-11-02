dc4e245ef96c5f1ea6b19dc3d82f2c666c6140e2
Wolfcoder13 <Wolfninja13@gmail.com>
2016-11-02 17:13:19 +0000

    started working on loginController and is implementation and services

---
+++ TimeThief/src/main/java/project/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package project.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class that has methods for String Manipulation
+ *
+ * DISCLAIMER:  This class is just for demonstration purposes,
+ *              something basic as this would be handled where it is needed
+ */
+@Service
+public class EmployeeService {
+
+}
