4f4e20b63d313392c9e3554bcf6d0e05a147690b
Örn Arnarson <orn@arnarson.net>
2016-11-06 22:43:17 +0000

    Add EmployeeController -- work in progress

---
+++ TimeThief/src/main/java/project/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package project.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import project.persistence.entities.Employee;
+import project.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	
+	// Instance Variables
+    EmployeeService employeeService;
+
+    // Dependency Injection
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    
+    @RequestMapping(value = "/employee/view/all", method = RequestMethod.GET)
+    public String loginPage(HttpSession session){
+
+        // The string "Login" that is returned here is the name of the view
+        // (the Login.jsp file) that is in the path /main/webapp/WEB-INF/jsp/
+        // If you change "Login" to something else, be sure you have a .jsp
+        // file that has the same name
+    	
+    	Long userId = (Long)session.getAttribute("loggedInUser");
+    	Employee currentEmployee = employeeService.findOne(userId);
+    	if (currentEmployee.getIsAdmin()) {
+    		// placeholder return -- TODO decide what to do
+    		return "employeelist";
+    	}
+    	else {
+    		// placeholder return -- TODO decide what to do
+    		return "unauthorized";
+    	}	
+    }
+}
